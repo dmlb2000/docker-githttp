253482a3303c2a0150eee3a761609b023efc4051
David Brown <dmlb2000@gmail.com>
2015-10-26 16:17:08 -0400

    add the dav lockfile into the right place

--- runit.sh
+++ runit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xe
 
+sed -i 's#${APACHE_LOCK_DIR}#/var/www#' /etc/apache2/mods-enabled/dav_fs.conf
 for dir in `ls /var/www/html/` ; do
   sed 's/@@DIR@@/'$dir'/g;s#@@PASSWD_FILE@@#'$PASSWD_FILE'#g' /templates/repo-template.conf > /etc/apache2/sites-available/git-$dir.conf
   a2ensite git-$dir
